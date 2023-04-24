6f159525131622c9e1bdbe9e6601dcb3db91cab8
S2Hyeon <tmdgus908@naver.com>
2023-04-24 17:34:29 +0900

    :sparkles: feat: CustomException 생성

---
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/common/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package co.ninuc.ninucco.common.exception;
+
+public class AccessDeniedException extends Exception {
+    public AccessDeniedException(String msg) {
+        super(msg);
+    }
+}
---
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/common/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package co.ninuc.ninucco.common.exception;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String msg) {
+        super(msg);
+    }
+}
