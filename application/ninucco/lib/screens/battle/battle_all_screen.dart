ffb854457aeb1ce2bd0eece59be3599be49dcb0a
dasliebeich7@gmail.com <dasliebeich7@gmail.com>
2023-04-24 17:13:18 +0900

    :sparkles: 배틀 공통 위젯 구현

---
+++ application/ninucco/lib/screens/battle/battle_all_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:ninucco/services/user_rank_api_service.dart';
+import 'package:ninucco/widgets/battle/battle_item_widget.dart';
+import 'package:ninucco/models/user_rank_info_model.dart';
+
+class BattleAllScreen extends StatelessWidget {
+  BattleAllScreen({super.key});
+
+  final Future<List<UserRankInfoModel>> userRanks =
+      UserRankApiService.getUserRanks();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: FutureBuilder(
+        future: userRanks,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Expanded(child: makeList(snapshot))
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<UserRankInfoModel>> snapshot) {
+    return ListView.separated(
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var userRank = snapshot.data![index];
+        return BattleItem(
+          memberAId: 1,
+          memberANickname: "장인훅",
+          memberAImage: userRank.profileImage,
+          memberBId: 1,
+          memberBNickname: "하훈묵",
+          memberBImage: userRank.profileImage,
+          battleId: userRank.id,
+          question: "누가 더 백엔드 개발자처럼 생겼나요?",
+          ratioA: 3.0,
+          ratioB: 1.3,
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
+    );
+  }
+}
