f97a4e84e3a26e8076468c6038ace1653f4f943d
v7153623 <71223238+v7153623@users.noreply.github.com>
2023-04-25 15:10:42 +0900

    ✨feat: 멤버 컨트롤러, 서비스, 서비스 impl, 레포지토리 추가

---
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/controller/MemberController.java
@@ -0,0 +1,12 @@
+package co.ninuc.ninucco.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+}
