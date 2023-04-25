9692403bad4eec19719cf6ccc6abdf23c3b74d6e
TraceofLight <rickyjun45@gmail.com>
2023-04-25 17:55:11 +0900

    :sparkles: feat: frame
    참조할 스크린 추가

---
+++ application/ninucco/lib/screens/login/login_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:ninucco/screens/login/login_google_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+  // final Future<List<UserRankInfoModel>> userRanks =
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        titleTextStyle: const TextStyle(color: Colors.black, fontSize: 20),
+        title: const Text('Sign In'),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(
+            child: LoginGoogleScreen(),
+          ),
+        ],
+      ),
+    );
+  }
+}
