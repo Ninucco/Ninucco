b58eb933baa72bcced22e9249818834d6a04c9d5
developerhongjulee <developerhongjulee@gmail.com>
2023-04-25 14:00:46 +0900

    :sparkles: feat: add BattleController, BattleService

--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/controller/BattleController.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/controller/BattleController.java
@@ -1,10 +1,14 @@
 package co.ninuc.ninucco.api.controller;
 
 import co.ninuc.ninucco.api.dto.ApiResult;
+import co.ninuc.ninucco.api.dto.request.BettingCreateReq;
 import co.ninuc.ninucco.api.dto.request.BattleCreateReq;
 import co.ninuc.ninucco.api.dto.request.CommentCreateReq;
 import co.ninuc.ninucco.api.service.BattleService;
+import co.ninuc.ninucco.api.service.CommentService;
 import co.ninuc.ninucco.common.exception.NotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,46 +18,64 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class BattleController {
     private final BattleService battleService;
+    private final CommentService commentService;
+    private final boolean SUCCESS = true;
 
     //배틀 등록
+    @ApiOperation(value = "배틀 등록", notes = "배틀을 등록합니다.")
     @PostMapping("/")
     public ResponseEntity<?> insertBattle(BattleCreateReq battleCreateReq) throws NotFoundException{
         return ResponseEntity.ok().body(
-                new ApiResult<>(true, battleService.insertBattle(battleCreateReq))
+                new ApiResult<>(SUCCESS, battleService.insertBattle(battleCreateReq))
         );
     }
     //배틀 리스트 조회
+    @ApiOperation(value = "배틀 리스트 조회", notes="배틀 리스트를 조회합니다. option: latest(최신순), votes(투표수 높은 순)")
     @GetMapping("/list")
     public ResponseEntity<?> selectAllBattle(@RequestParam String option){
         return ResponseEntity.ok().body(
-                new ApiResult<>(true, battleService.selectAllBattle(option))
+                new ApiResult<>(SUCCESS, battleService.selectAllBattle(option))
         );
     }
     //배틀 상세정보 조회
+    @ApiOperation(value = "배틀 상세정보 조회", notes = "배틀 진행중의 상세정보를 조회합니다.")
     @GetMapping("/{battleId}")
-    public ResponseEntity<?> selectOneBattle(Long battleId) throws NotFoundException{
+    public ResponseEntity<?> selectOneBattle(@PathVariable Long battleId) throws NotFoundException{
         return ResponseEntity.ok().body(
-                new ApiResult<>(true, battleService.selectOneBattle(battleId))
+                new ApiResult<>(SUCCESS, battleService.selectOneBattle(battleId))
         );
     }
     //댓글 작성
+    @ApiOperation(value="댓글 작성", notes = "댓글 작성")
     @PostMapping("/comment")
     public ResponseEntity<?> insertComment(CommentCreateReq commentCreateReq) throws NotFoundException{
-        return null;
+        return ResponseEntity.ok().body(
+                new ApiResult<>(SUCCESS, commentService.insertComment(commentCreateReq))
+        );
     }
-
-
-
-//    @Override
-//    public void updateArticle(ArticleUpdate param) throws NotFoundException{
-//        Article article = articleRepository.findById(param.getArticleId())
-//                .orElseThrow(()->new NotFoundException("articleNo가 존재하지 않습니다"));
-//        article.updateArticle(param.getContent());
-//    }
-//
-//    @Override
-//    public void deleteArticle(Long articleId) throws NotFoundException{
-//        articleRepository.deleteById(articleId);
-//    }
+    //댓글 리스트 조회
+    @ApiOperation(value="댓글 리스트 조회", notes = "배틀의 댓글 리스트를 조회합니다.")
+    @GetMapping("/{battleId}/comment")
+    public ResponseEntity<?> selectAllComment(@PathVariable Long battleId) throws NotFoundException{
+        return ResponseEntity.ok().body(
+          new ApiResult<>(SUCCESS, commentService.selectAllComment(battleId))
+        );
+    }
+    //배팅하기
+    @ApiOperation(value="배팅하기", notes = "배팅 신청")
+    @PostMapping("/bet")
+    public ResponseEntity<?> insertBetting(@RequestBody BettingCreateReq bettingCreateReq) throws NotFoundException {
+        return ResponseEntity.ok().body(
+          new ApiResult<>(SUCCESS, battleService.insertBetting(bettingCreateReq))
+        );
+    }
+    //배틀 결과 조회: 끝난 배틀 조회
+    @ApiOperation(value="배틀 결과 조회", notes = "끝난 배틀")
+    @GetMapping("/battle/{battleId}/result")
+    public ResponseEntity<?> selectOneBattleResult(@PathVariable Long battleId) throws NotFoundException{
+        return ResponseEntity.ok().body(
+                new ApiResult<>(SUCCESS, battleService.selectOneBattleResult(battleId))
+        );
+    }
 
 }
--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/BattleService.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/BattleService.java
@@ -1,4 +1,17 @@
 package co.ninuc.ninucco.api.service;
 
+import co.ninuc.ninucco.api.dto.request.BattleCreateReq;
+import co.ninuc.ninucco.api.dto.request.BettingCreateReq;
+import co.ninuc.ninucco.api.dto.response.BattleRes;
+import co.ninuc.ninucco.api.dto.response.BattleResultRes;
+import co.ninuc.ninucco.common.exception.NotFoundException;
+
+import java.util.List;
+
 public interface BattleService {
+    Long insertBattle(BattleCreateReq battleCreateReq) throws NotFoundException;
+    List<BattleRes> selectAllBattle(String option);
+    BattleRes selectOneBattle(Long battleId) throws NotFoundException;
+    Long insertBetting(BettingCreateReq battleCreateReq) throws NotFoundException;
+    BattleResultRes selectOneBattleResult(Long battleId) throws NotFoundException;
 }
