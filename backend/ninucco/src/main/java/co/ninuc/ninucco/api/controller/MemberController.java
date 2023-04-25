6164a7f716b53e241425f2c77107ea44ecc0ea57
v7153623 <71223238+v7153623@users.noreply.github.com>
2023-04-25 15:12:40 +0900

    ♻refactor: api --

--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/controller/MemberController.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/controller/MemberController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
