ab20cc0289f07ee5479202f40844cae83a7bcf43
developerhongjulee <developerhongjulee@gmail.com>
2023-04-26 17:57:42 +0900

    :sparkles: feat: BattleServiceImpl 기능 개발(배틀 종료 제외)

--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/BattleServiceImpl.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/BattleServiceImpl.java
@@ -8,59 +8,64 @@ import co.ninuc.ninucco.api.dto.response.BattleResultRes;
 import co.ninuc.ninucco.common.exception.CustomException;
 import co.ninuc.ninucco.db.entity.Battle;
 import co.ninuc.ninucco.db.entity.Betting;
+import co.ninuc.ninucco.db.entity.Member;
 import co.ninuc.ninucco.db.repository.BattleRepository;
+import co.ninuc.ninucco.db.repository.BettingRepository;
 import co.ninuc.ninucco.db.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BattleServiceImpl implements BattleService{
     private final BattleRepository battleRepository;
     private final MemberRepository memberRepository;
+    private final BettingRepository bettingRepository;
 
     @Transactional
     @Override
     public Long insertBattle(BattleCreateReq battleCreateReq){
-        return null;
+        return battleRepository.save(toEntity(battleCreateReq)).getId();
     }
 
     @Override
     public List<BattleRes> selectAllBattle(String option) {
-        return null;
+        return battleRepository.findAll().stream()
+                .map(this::toRes).collect(Collectors.toList());
     }
 
     @Override
     public BattleRes selectOneBattle(Long battleId){
-        return null;
+        return toRes(battleRepository.findById(battleId)
+                .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_BATTLE)));
     }
 
     @Transactional
     @Override
-    public Long insertBetting(BettingCreateReq battleCreateReq){
-        return null;
+    public Long insertBetting(BettingCreateReq bettingCreateReq){
+        return bettingRepository.save(toEntity(bettingCreateReq)).getId();
     }
 
+    //TODO: 배틀 결과 조회 필요?
     @Override
     public BattleResultRes selectOneBattleResult(Long battleId){
         return null;
     }
-    //시간마다 배틀 끝났는지 체크
-    //시간 끝났으면 배틀 끝내고 플러터로 알림 보내기
-
-
+    //TODO: 시간마다 배틀 끝났는지 체크
+    //TODO: 시간 끝났으면 배틀 끝내고 플러터로 알림 보내기
 
     // toEntity, toRes
     Battle toEntity(BattleCreateReq battleCreateReq){
         return Battle.builder()
                 .title(battleCreateReq.getTitle())
                 .applicant(memberRepository.findById(battleCreateReq.getApplicantId())
-                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_A)))
+                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_MEMBER)))
                 .opponent(memberRepository.findById(battleCreateReq.getOpponentId())
-                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_A)))
+                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_MEMBER)))
                 .applicantUrl(battleCreateReq.getApplicantUrl())
                 .opponentUrl(battleCreateReq.getOpponentUrl())
                 .applicantOdds(1.0)
@@ -69,10 +74,25 @@ public class BattleServiceImpl implements BattleService{
     Betting toEntity(BettingCreateReq bettingCreateReq){
         return Betting.builder()
                 .battle(battleRepository.findById(bettingCreateReq.getBattleId())
-                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_A)))
+                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_MEMBER)))
                 .member(memberRepository.findById(bettingCreateReq.getMemberId())
-                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_A)))
+                        .orElseThrow(()->new CustomException(ErrorRes.NOT_FOUND_MEMBER)))
                 .betSide(bettingCreateReq.getBetSide())
                 .betMoney(bettingCreateReq.getBetMoney()).build();
+    }
+    BattleRes toRes(Battle battle){
+        Member applicant = battle.getApplicant();
+        Member opponent = battle.getOpponent();
+        return BattleRes.builder()
+                .battleId(battle.getId())
+                .applicantName(applicant.getNickname())
+                .opponentName(opponent.getNickname())
+                .title(battle.getTitle())
+                .applicantUrl(battle.getApplicantUrl())
+                .opponentUrl(battle.getOpponentUrl())
+                .currentVotes(bettingRepository.countByBattleId(battle.getId()))
+                .applicantOdds(battle.getApplicantOdds())
+                .opponentOdds(battle.getOpponentOdds())
+                .finishTime(battle.getFinishAt()).build();
     }
 }
--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/CommentServiceImpl.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/CommentServiceImpl.java
@@ -1,7 +1,20 @@
 package co.ninuc.ninucco.api.service;
 
+import co.ninuc.ninucco.api.dto.request.CommentCreateReq;
+import co.ninuc.ninucco.api.dto.response.CommentRes;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentServiceImpl implements CommentService{
+    @Override
+    public Long insertComment(CommentCreateReq commentCreateReq) {
+        return null;
+    }
+
+    @Override
+    public List<CommentRes> selectAllComment(Long battleId) {
+        return null;
+    }
 }
