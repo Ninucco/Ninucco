4de6ff621ea756ab3e59e0c106aeff36199dc7e7
developerhongjulee <developerhongjulee@gmail.com>
2023-04-27 10:49:10 +0900

    :sparkles: feat: remove duplicate functions, add existsByNickName

--- backend/ninucco/src/main/java/co/ninuc/ninucco/db/repository/MemberRepository.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/db/repository/MemberRepository.java
@@ -3,9 +3,6 @@ package co.ninuc.ninucco.db.repository;
 import co.ninuc.ninucco.db.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, String> {
-    Optional<Member> findMemberById(String Id);
-    Optional<Member> findMemberByNickname(String nickname);
+    Boolean existsByNickname(String nickname);
 }
