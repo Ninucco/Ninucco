ff354f4415146e2b49daec0ea4c02f6df2c5f016
v7153623 <71223238+v7153623@users.noreply.github.com>
2023-04-28 17:31:59 +0900

    ✨ feat: 닉네임, 사진 변경 controller에 추가

--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/dto/request/MemberCreateReq.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/dto/request/MemberCreateReq.java
@@ -1,12 +1,10 @@
 package co.ninuc.ninucco.api.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor
 public class MemberCreateReq {
     String id;
 
