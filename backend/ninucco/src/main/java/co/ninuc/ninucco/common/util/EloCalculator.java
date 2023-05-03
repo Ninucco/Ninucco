c89f752d72356a0471b085ae73c55e7f7442f774
HongjuLee <wx776654@gmail.com>
2023-05-03 13:57:13 +0900

    :sparkles: feat: add EloCalculator

---
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/common/util/EloCalculator.java
@@ -0,0 +1,36 @@
+package co.ninuc.ninucco.common.util;
+
+import co.ninuc.ninucco.api.dto.ErrorRes;
+import co.ninuc.ninucco.common.exception.CustomException;
+import co.ninuc.ninucco.db.entity.Member;
+import co.ninuc.ninucco.db.entity.type.BattleResult;
+
+public class EloCalculator {
+    static final int C = 20;
+    //승패에 따라 Elo점수 반영
+    public static void updateEloByResult(Member applicant, Member opponent, BattleResult winner){
+        Member mWin, mLose;
+        if(winner==BattleResult.APPLICANT) {
+            mWin = applicant;
+            mLose = opponent;
+        } else if (winner==BattleResult.OPPONENT) {
+            mWin = opponent;
+            mLose = applicant;
+        }else throw new CustomException(ErrorRes.INTERNAL_SERVER_ERROR);
+        int rLose = mLose.getRate();
+        int rSum = mWin.getRate()+rLose;
+        int d = (int)((double)C*2*rLose/rSum);
+        if(d==0) d=1;
+        mWin.updateRate(mWin.getRate()+d);
+        mLose.updateRate(mLose.getRate()-d);
+    }
+    // 배당 구하기
+    public static double[] calcOdds(Member m1, Member m2){
+        int r1=m1.getRate();
+        int r2=m2.getRate();
+        int rSum = r1+r2;
+        double o1 = 1+(double)2*r2/rSum;
+        double o2 = 1+(double)2*r1/rSum;
+        return new double[] {o1,o2};
+    }
+}
