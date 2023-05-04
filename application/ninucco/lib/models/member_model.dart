b0f1a95b97b700d0fcef879abc762b1364f14363
TraceofLight <rickyjun45@gmail.com>
2023-05-04 18:04:43 +0900

    :sparkles: feat: model
    모델 내용 추가

---
+++ application/ninucco/lib/models/member_model.dart
@@ -0,0 +1,15 @@
+import 'package:ninucco/providers/auth_provider.dart';
+
+class MemberModel {
+  final String id, nickname, url;
+  final BigInt winCount, loseCount, point, elo;
+
+  MemberModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        nickname = json['nickname'],
+        url = json['url'],
+        winCount = json['winCount'],
+        loseCount = json['loseCount'],
+        point = json['point'],
+        elo = json['elo'];
+}
