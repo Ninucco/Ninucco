cddc155e77a309516faa76d09675cd5e5f927e66
developerhongjulee <developerhongjulee@gmail.com>
2023-05-05 21:03:57 +0900

    :sparkles: feat: add get_sefs(), get_models_and_classes, predict(img_dir, _model, _class)

--- backend/ninucco-model/model.py
+++ backend/ninucco-model/model.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 import s3fs
 import tensorflow
-# from pathlib import Path
 import numpy as np
 import tempfile
 
@@ -15,44 +14,64 @@ BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-model = []
-classes = []
+model_names = ["animal", "fruit", "highschool", "job"]
+
 
 def get_s3fs():
     return s3fs.S3FileSystem(key=AWS_ACCESS_KEY, secret=AWS_SECRET_KEY)
 
-def s3_set_keras_model_and_classes(model_type: str):
-    global model
-    global classes
+
+def get_models_and_classes():
+    global model_names
+    models = {}
+    classes = {}
 
     with tempfile.TemporaryDirectory() as tempdir:
         s3fs = get_s3fs()
-        s3fs.get(f"{BUCKET_NAME}/model/{model_type}/", f"{tempdir}/model/{model_type}/", recursive=True)
-        model = tensorflow.keras.models.load_model(f"{tempdir}/model/{model_type}/keras_model.h5", compile=False)
+        s3fs.get(f"{BUCKET_NAME}/model/",
+                 f"{tempdir}/model/", recursive=True)
+        for model_name in model_names:
+            models[model_name] = tensorflow.keras.models.load_model(
+                f"{tempdir}/model/{model_name}/keras_model.h5", compile=False)
 
-        lables_path = f"{tempdir}/model/{model_type}/labels.txt"
-        lables_file = open(lables_path, encoding='UTF-8')
+            classes[model_name] = []
+            lables_path = f"{tempdir}/model/{model_name}/labels.txt"
+            lables_file = open(lables_path, encoding='UTF-8')
 
-        classes = []
-        line = lables_file.readline()
-        while line:
-            classes.append(line.split(' ', 1)[1].rstrip())
             line = lables_file.readline()
-        lables_file.close()
-def main():
-    global model
-    global classes
+            while line:
+                classes[model_name].append(line.split(' ', 1)[1].rstrip())
+                line = lables_file.readline()
+            lables_file.close()
+        print(models.keys())
+        return models, classes
 
-    s3_set_keras_model_and_classes("animal")
 
-    image = tensorflow.keras.utils.load_img('10241024.jpg', target_size=(224,224))
+def predict(img_dir, _model, _class):
+    image = tensorflow.keras.utils.load_img(
+        img_dir, target_size=(224, 224))
     input_arr = tensorflow.keras.utils.img_to_array(image)
     input_arr = np.array([input_arr])
-    result = model.predict(input_arr)[0]
-    
-    for i in range(0,len(result)):
-        print(f"{classes[i]}: {result[i]}")
-    
+
+    result = _model.predict(input_arr)[0]
+
+    similarity_result = {}
+    for i in range(0, len(result)):
+        similarity_result[_class[i]] = result[i]
+    similarity_result = {k: v for k, v in sorted(
+        similarity_result.items(), key=lambda item: item[1], reverse=True)}
+    return similarity_result
+
+
+def main():
+    models = {}
+    classes = {}
+
+    models, classes = get_models_and_classes()
+    similarity_result = predict(
+        '10241024.png', models['job'], classes['job'])
+    print(similarity_result)
+
 
 if __name__ == '__main__':
     main()
