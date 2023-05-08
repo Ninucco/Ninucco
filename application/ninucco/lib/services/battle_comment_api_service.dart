0069e464cbb3801d734220f88c18c6d2bce3bc27
dasliebeich7@gmail.com <dasliebeich7@gmail.com>
2023-05-08 15:28:58 +0900

    :sparkles: 배틀, 배틀 상세 api 갈아낌

--- application/ninucco/lib/services/battle_comment_api_service.dart
+++ application/ninucco/lib/services/battle_comment_api_service.dart
@@ -4,19 +4,21 @@ import 'package:http/http.dart' as http;
 import 'package:ninucco/models/battle_comment_info_model.dart';
 
 class BattleApiCommentService {
-  static const String baseUrl = "https://dummyjson.com";
+  static const String baseUrl = "https://k8a605.p.ssafy.io/api/battle";
 
   static Stream<List<BattleCommentInfoModel>> getBattleComments(
       int battleId) async* {
     List<BattleCommentInfoModel> battleInstances = [];
     // await Future.delayed(const Duration(milliseconds: 1000));
-    final url = Uri.parse('$baseUrl/users');
+    final url = Uri.parse('$baseUrl/$battleId/comment');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      final battles = jsonDecode(response.body)["users"];
-      for (var battle in battles) {
-        final instance = BattleCommentInfoModel.fromJson(battle);
-        battleInstances.add(instance);
+      final battles = jsonDecode(response.body)["data"]["commentList"];
+      if (battles != null) {
+        for (var battle in battles) {
+          final instance = BattleCommentInfoModel.fromJson(battle);
+          battleInstances.add(instance);
+        }
       }
       yield battleInstances;
     }
