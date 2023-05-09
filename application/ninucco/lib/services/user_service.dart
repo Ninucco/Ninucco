06e4773bde991856e63fe77eecc7fb1ee9a6bd50
훈목 <hunmok1027@gmail.com>
2023-05-09 16:06:44 +0900

    ✨ feat: userDetail API호출

--- application/ninucco/lib/services/user_service.dart
+++ application/ninucco/lib/services/user_service.dart
@@ -1,9 +1,22 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:ninucco/models/user_detail_model.dart';
 import 'package:ninucco/models/user_model.dart';
 
-class UserSearchApiService {
+class UserService {
+  static Future<UserDetailData> getUserDetailById() async {
+    const String baseUrl = "http://70.12.246.242:4000";
+    final url = Uri.parse('$baseUrl/testuser');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final userDetail = jsonDecode(response.body);
+
+      return UserDetailData.fromJson(userDetail);
+    }
+    throw Error();
+  }
+
   static Future<List<UserModel>> searchUserByNickname(String keyword) async {
     const String baseUrl = "https://k8a605.p.ssafy.io/api/member/search";
     List<UserModel> userList = [];
