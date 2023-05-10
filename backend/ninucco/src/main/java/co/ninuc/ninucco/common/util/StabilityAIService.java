9b7e657d630aeb44dec0f48ef2e6ec977e0c6bb3
developerhongjulee <developerhongjulee@gmail.com>
2023-05-10 13:56:37 +0900

    :bug: fix: move url to application.yml

--- backend/ninucco/src/main/java/co/ninuc/ninucco/common/util/StabilityAIService.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/common/util/StabilityAIService.java
@@ -1,7 +1,6 @@
 package co.ninuc.ninucco.common.util;
 
 import co.ninuc.ninucco.api.dto.ErrorRes;
-import co.ninuc.ninucco.api.dto.interservice.PromptToImgReq;
 import co.ninuc.ninucco.common.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Headers;
@@ -18,38 +17,19 @@ import java.util.Optional;
 @Component
 @Slf4j
 public class StabilityAIService{
-    @Value("{ai.stability.url}")
-    String url;
+    private final String url;
     private final InterServiceCommunicationProvider isp;
-//    private final Headers promptToImgHeaders;
     private final Headers imgToImgHeaders;
-    public StabilityAIService(@Value("${ai.stability.key}") String stabilityAIKey) {
+    public StabilityAIService(@Value("${ai.stability.key}") String stabilityAIKey,
+    @Value("${ai.stability.url}") String stabilityAIUrl) {
         this.isp = new InterServiceCommunicationProvider();
-//        this.promptToImgHeaders = Headers.of(
-//                "Content-Type","application/json",
-//                "Accept","application/json",
-//                "Authorization", stabilityAIKey
-//        );
+        this.url = stabilityAIUrl;
         this.imgToImgHeaders = Headers.of(
                 "Content-Type","multipart/form-data",
                 "Accept","application/json",
                 "Authorization", stabilityAIKey
         );
     }
-
-//    private JSONObject getJsonObjectPromptToImg(String prompt){
-//        Optional<JSONObject> res = isp.postRequestSendJsonGetJsonObject("https://api.stability.ai/v1/generation/stable-diffusion-xl-beta-v2-2-2/image-to-image",
-//                promptToImgHeaders,
-//                new PromptToImgReq(prompt));
-//        if(res.isEmpty()) throw new CustomException(ErrorRes.INTERNAL_SERVER_ERROR_FROM_STABILITY_AI);
-//        else return res.get();
-//    }
-//    public byte[] getByteArrayPromptToImg(String prompt){
-//        JSONArray picArray = (JSONArray) this.getJsonObjectPromptToImg(prompt).get("artifacts");
-//        JSONObject fstPic = (JSONObject)picArray.get(0);
-//        String fstPicBase64 = (String)fstPic.get("base64");
-//        return Base64.decodeBase64(fstPicBase64);
-//    }
     private JSONObject getJsonObjectImgToImg(byte[] imgByteArray, String prompt){
         RequestBody requestBody = new MultipartBody.Builder()
                 .addFormDataPart("init_image", "", RequestBody.create(imgByteArray, MediaType.parse("image/png")))
@@ -65,9 +45,7 @@ public class StabilityAIService{
             .build();
         log.info(requestBody.contentType().toString());
 
-        Optional<JSONObject> res = isp.postRequestSendRequestbodyGetJsonObject(url,
-                imgToImgHeaders,requestBody
-                );
+        Optional<JSONObject> res = isp.postRequestSendRequestbodyGetJsonObject(url, imgToImgHeaders, requestBody);
         if(res.isEmpty()) throw new CustomException(ErrorRes.INTERNAL_SERVER_ERROR_FROM_STABILITY_AI);
         else return res.get();
     }
