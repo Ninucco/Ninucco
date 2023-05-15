b1505431d892b0de6a9e1d43e1f97d5b56b89e00
v7153623 <71223238+v7153623@users.noreply.github.com>
2023-05-15 15:17:45 +0900

    :sparkles: feat: 포인트 랭킹, elo 랭킹 추가

--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/controller/RankingController.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/controller/RankingController.java
@@ -18,9 +18,21 @@ public class RankingController {
     private final boolean SUCCESS = true;
     private final RankingService rankingService;
 
-    @ApiOperation(value = "배틀 이긴 횟수 랭킹 조회", notes = "배틀 이긴 횟수 TOP10 랭킹을 조회합니다.")
+    @ApiOperation(value = "배틀 이긴 횟수 랭킹 조회", notes = "배틀 이긴 횟수 TOP100 랭킹을 조회합니다.")
     @GetMapping("/battle")
     public ResponseEntity<ApiResult<Res>> selectBattleRanking(){
         return ResponseEntity.ok().body(new ApiResult<>(SUCCESS, rankingService.selectBattleRanking()));
+    }
+
+    @ApiOperation(value = "elo 랭킹 조회", notes = "elo TOP100 랭킹을 조회합니다.")
+    @GetMapping("/elo")
+    public ResponseEntity<ApiResult<Res>> selectEloRanking(){
+        return ResponseEntity.ok().body(new ApiResult<>(SUCCESS, rankingService.selectEloRanking()));
+    }
+
+    @ApiOperation(value = "point 랭킹 조회", notes = "point TOP100 랭킹을 조회합니다.")
+    @GetMapping("/point")
+    public ResponseEntity<ApiResult<Res>> selectPointRanking(){
+        return ResponseEntity.ok().body(new ApiResult<>(SUCCESS, rankingService.selectPointRanking()));
     }
 }
--- backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/RankingServiceImpl.java
+++ backend/ninucco/src/main/java/co/ninuc/ninucco/api/service/RankingServiceImpl.java
@@ -1,7 +1,11 @@
 package co.ninuc.ninucco.api.service;
 
 import co.ninuc.ninucco.api.dto.BattleRankingListInfo;
+import co.ninuc.ninucco.api.dto.EloRankingListInfo;
+import co.ninuc.ninucco.api.dto.PointRankingListInfo;
 import co.ninuc.ninucco.api.dto.response.BattleRankingRes;
+import co.ninuc.ninucco.api.dto.response.EloRankingRes;
+import co.ninuc.ninucco.api.dto.response.PointRankingRes;
 import co.ninuc.ninucco.api.dto.response.SimilarityRankingRes;
 import co.ninuc.ninucco.db.entity.Member;
 import co.ninuc.ninucco.db.repository.MemberRepository;
@@ -18,8 +22,19 @@ public class RankingServiceImpl implements RankingService {
 
     @Override
     public BattleRankingRes selectBattleRanking() {
-        return new BattleRankingRes(memberRepository.findTop10ByOrderByWinCountDesc().stream().map(this::toBattleRankingRes).collect(Collectors.toList()));
+        return new BattleRankingRes(memberRepository.findTop100ByOrderByWinCountDesc().stream().map(this::toBattleRankingRes).collect(Collectors.toList()));
     }
+
+    @Override
+    public EloRankingRes selectEloRanking() {
+        return new EloRankingRes(memberRepository.findTop100ByOrderByEloDesc().stream().map(this::toEloRankingRes).collect(Collectors.toList()));
+    }
+
+    @Override
+    public PointRankingRes selectPointRanking() {
+        return new PointRankingRes(memberRepository.findTop100ByOrderByPointDesc().stream().map(this::toPointRankingRes).collect(Collectors.toList()));
+    }
+
 
     @Override
     public SimilarityRankingRes selectSimilarityRanking() {
@@ -33,6 +48,23 @@ public class RankingServiceImpl implements RankingService {
                 .profileImage(member.getUrl())
                 .nickname(member.getNickname())
                 .winCount(member.getWinCount())
+                .build();
+    }
+
+    EloRankingListInfo toEloRankingRes(Member member){
+        return EloRankingListInfo.builder()
+                .memberId(member.getId())
+                .profileImage(member.getUrl())
+                .nickname(member.getNickname())
+                .elo(member.getElo())
+                .build();
+    }
+    PointRankingListInfo toPointRankingRes(Member member){
+        return PointRankingListInfo.builder()
+                .memberId(member.getId())
+                .profileImage(member.getUrl())
+                .nickname(member.getNickname())
+                .point(member.getPoint())
                 .build();
     }
 }
