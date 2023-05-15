24271482581494c862f18f1ec4dd3f2350043e43
훈목 <hunmok1027@gmail.com>
2023-05-15 16:47:21 +0900

    ✨ feat: update Profile API

--- application/ninucco/lib/services/user_service.dart
+++ application/ninucco/lib/services/user_service.dart
@@ -41,4 +41,28 @@ class UserService {
 
     return jsonDecode(response.body)['data']['validate'];
   }
+
+  static void updateProfile({
+    required String memberId,
+    required String nickname,
+    required String profileUrl,
+  }) async {
+    // 닉네임 변경
+    final url = Uri.parse("https://k8a605.p.ssafy.io/api/member/nickname");
+    Map data = {"id": memberId, "nickname": nickname};
+    var body = json.encode(data);
+    http.patch(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+
+    // 프로필 이미지 변경
+    final imageUrl =
+        Uri.parse("https://k8a605.p.ssafy.io/api/member/photo/url");
+    final request = http.MultipartRequest("PATCH", imageUrl);
+    request.fields["memberId"] = memberId;
+    request.fields["url"] = profileUrl;
+    request.send();
+  }
 }
