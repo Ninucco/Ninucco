4495ef9916a361617cc2304cc0e702548c1d4361
TraceofLight <rickyjun45@gmail.com>
2023-05-19 03:31:14 +0900

    :sparkles: feat: server setting
    서버 세팅 변경

--- frontend/ninucco/vite.config.ts
+++ frontend/ninucco/vite.config.ts
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
     port: 3000,
+    open: true,
   },
   plugins: [react()],
 });
