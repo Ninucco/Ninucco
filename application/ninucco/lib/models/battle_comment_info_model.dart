f2599562f1b4aeb9fcd31fff426ef19e2611134b
dasliebeich7@gmail.com <dasliebeich7@gmail.com>
2023-05-18 13:49:34 +0900

    bug: 댓글 시간 추가

--- application/ninucco/lib/models/battle_comment_info_model.dart
+++ application/ninucco/lib/models/battle_comment_info_model.dart
@@ -1,16 +1,19 @@
 class BattleCommentInfoModel {
   final String profileImage, nickname, content, id;
+  final List<dynamic> createdAt;
 
   BattleCommentInfoModel({
     required this.content,
     required this.profileImage,
     required this.id,
     required this.nickname,
+    required this.createdAt,
   });
 
   BattleCommentInfoModel.fromJson(Map<String, dynamic> json)
       : id = json["memberId"],
         profileImage = json['profileImage'],
         nickname = json['nickname'],
-        content = json['content'];
+        content = json['content'],
+        createdAt = json['createdAt'];
 }
