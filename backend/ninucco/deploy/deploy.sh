2090de95e5608a6f60429623c486331ec59b627f
S2Hyeon <tmdgus908@naver.com>
2023-06-02 15:23:25 +0900

    :bug: fix: deploy.sh 수정(서버이전에 따른 주소, 로직 변경)

--- backend/ninucco/deploy/deploy.sh
+++ backend/ninucco/deploy/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 APP_NAME=ninucco_back
- 
+
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
 EXIST_BLUE=$(docker-compose -p ${APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
- 
+
 # 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
@@ -16,9 +16,9 @@ else
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
 fi
- 
+
 sleep 15
- 
+
 # 새로운 컨테이너가 제대로 떴는지 확인
 SECONDS=0
 while :
@@ -26,12 +26,13 @@ do
   EXIST_AFTER=$(docker-compose -p ${APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
   echo "EXIST_AFTER : ${EXIST_AFTER}"
   if [ -n "$EXIST_AFTER" ]; then
-	  TEST_API_STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://k8a605.p.ssafy.io/api/rank/battle)
+	  TEST_API_STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://ninucco.com/api/rank/battle)
     echo "TEST_API_STATUS_CODE : ${TEST_API_STATUS_CODE}"
     if [ "$TEST_API_STATUS_CODE" == 200 ]; then
       echo "TEST API SUCCESS !! >> ${AFTER_COMPOSE_COLOR} Container WAS Running!"
 
-      file_path="../settings/r_proxy/nginx.all.conf"
+      # 포트 번호 수정
+      file_path="/home/ubuntu/mount/sites-available/default.conf"
       if [ "$BEFORE_COMPOSE_COLOR" == "green" ]; then
         old_string="8082"
         new_string="8081"
@@ -42,11 +43,11 @@ do
       sed -i "s/$old_string/$new_string/g" "$file_path"
       echo "switch port : $old_string to $new_string"
 
-      # 새로운 컨테이너에 대한 nginx 설정파일 복사 후 nginx reload
-      echo "COPY to container"
-      docker cp ${file_path} r_proxy:/etc/nginx/conf.d/default.conf
+      # echo "COPY to container"
+      # docker cp ${file_path} r_proxy:/etc/nginx/conf.d/default.conf
+
       echo "reload nginx"
-      docker exec r_proxy service nginx reload
+      docker exec nginx service nginx reload
 
       # 이전 컨테이너 종료
       docker-compose -p ${APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
